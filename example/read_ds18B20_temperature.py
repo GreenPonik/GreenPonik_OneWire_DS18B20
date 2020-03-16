5c66a012541d1db109cfda5b30c38188d740054c
MkLHX <mickael.lehoux@gmail.com>
2020-03-16 13:48:24 +0100

    edit typo

--- example/read_ds18B20_temperature.py
+++ example/read_ds18B20_temperature.py
@@ -1,10 +1,10 @@
 import time
-from GreenPonik_OneWire_DS18B20 import read_temps
+from GreenPonik_OneWire_DS18B20 import GreenPonik_OneWire_DS18B20
 
 if __name__ == "__main__":
     while True:
         # read both celcius and fahrenheit temperatures
-        temperatures = read_temps()
+        temperatures = GreenPonik_OneWire_DS18B20.read_temps()
         print("celcius temp %.3f" % temperatures[0])
         print("fahrenheit temp %.3f" % temperatures[1])
         time.sleep(1)
