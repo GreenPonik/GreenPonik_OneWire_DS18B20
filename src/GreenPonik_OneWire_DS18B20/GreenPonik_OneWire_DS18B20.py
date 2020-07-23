a6cedba3adeac6cdd1ace2b25f7f18a47821d415
MkLHX <mickael.lehoux@gmail.com>
2020-07-23 16:54:42 +0200

    no class

--- src/GreenPonik_OneWire_DS18B20/GreenPonik_OneWire_DS18B20.py
+++ src/GreenPonik_OneWire_DS18B20/GreenPonik_OneWire_DS18B20.py
@@ -18,7 +18,6 @@ os.system('modprobe w1-therm')
 ONE_WIRE_PATH = '/sys/bus/w1/devices/'
 
 
-# class GreenPonik_OneWire_DS18B20:
 def __find_1w_sensor(self):
     files = [name for name in os.listdir(ONE_WIRE_PATH)]
     # if file not create wait 30sec an retry
