073dc77571a0449701cc0fd2155f4bcf517034d0
MkLHX <mickael.lehoux@gmail.com>
2020-07-23 17:10:16 +0200

    fix removed class

--- src/GreenPonik_OneWire_DS18B20/GreenPonik_OneWire_DS18B20.py
+++ src/GreenPonik_OneWire_DS18B20/GreenPonik_OneWire_DS18B20.py
@@ -18,14 +18,14 @@ os.system('modprobe w1-therm')
 ONE_WIRE_PATH = '/sys/bus/w1/devices/'
 
 
-def __find_1w_sensor(self):
+def __find_1w_sensor():
     files = [name for name in os.listdir(ONE_WIRE_PATH)]
     # if file not create wait 30sec an retry
     if(len(files) == 0):
         print('w1 file not found wait 30sec an retry')
         time.sleep(30)
         # relaunch script
-        self.__find_1w_sensor()
+        __find_1w_sensor()
     else:
         for file in files:
             print("one wire file found: %s" % file)
@@ -35,20 +35,20 @@ def __find_1w_sensor(self):
         return temp_sensor
 
 
-def __temp_raw(self):
+def __temp_raw():
     # temp_raw() -> read one wire lines
-    temp_sensor = self.__find_1w_sensor()
+    temp_sensor = __find_1w_sensor()
     f = open(temp_sensor, 'r')
     lines = f.readlines()
     f.close()
     return lines
 
 
-def read_temps(self):
-    lines = self.__temp_raw()
+def read_temps():
+    lines = __temp_raw()
     while lines[0].strip()[-3:] != 'YES':
         time.sleep(0.2)
-        lines = self.__temp_raw()
+        lines = __temp_raw()
 
     temp_output = lines[1].find('t=')
     if temp_output != 1:
